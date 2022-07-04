4f3015b0f8100348786fe1db47f90668d34dd75e
marindraganov <marodraganov@gmail.com>
2022-07-05 00:21:22 +0300

    Remove unused files. Test key for HEROKU

--- index.js
+++ index.js
@@ -1,28 +1,11 @@
-// Copyright 2020 Google LLC
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     https://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
-// [START cloudrun_helloworld_service]
-// [START run_helloworld_service]
 require('dotenv').config();
 const express = require('express');
 const ethers = require("ethers");
 const app = express();
 
 app.get('/', (req, res) => {
-  const name = process.env.NAME || 'World';
-  const provider = new ethers.ethers.providers.InfuraProvider("rinkeby", "40c2813049e44ec79cb4d7e0d18de173")
-  res.send(provider);
+  let testKey = process.env.TEST_PRIVATE_KEY;
+  res.send("Hello to The ERC20Validator" + testKey);
 });
 
 app.get('/mint', (req, res) => {
@@ -95,10 +78,7 @@ const getSigner = (targetChainID) => {
 const port = parseInt(process.env.PORT) || 8080;
 app.listen(port, () => {
   
-  console.log(`helloworld: listening on port ${port}`);
 });
-// [END run_helloworld_service]
-// [END cloudrun_helloworld_service]
 
-// Exports for testing purposes.
+
 module.exports = app;
